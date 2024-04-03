3e395da81a42433167dfb572d099d33eb607e4de
Bob Winter <39530207+winterb721@users.noreply.github.com>
2024-04-03 12:46:28 -0400

    added signin component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SigninComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { SigninComponent } from './signin/signin.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {path:'', redirectTo: 'signin', pathMatch: 'full'},
+     {path:'signin', component:SigninComponent}
+    ];
