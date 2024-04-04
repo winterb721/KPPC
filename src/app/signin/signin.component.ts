f7b5f486dd61004c0f60366976666cdfcbbd4cf1
Bob Winter <39530207+winterb721@users.noreply.github.com>
2024-04-04 10:47:10 -0400

    Added css for signin

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signin.component.html',
   styleUrl: './signin.component.css'
 })
 export class SigninComponent {
+  signinForm = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', [Validators.required, Validators.minLength(8)])
+  });
+  onSubmit() {
+    
+    //console.warn(this.signinForm.value);
+    console.log("You Entered Username: ", 
+      this.signinForm.controls['username'].value, 
+      "and password: ", this.signinForm.controls['password'].value)
 
-}
+    alert("You Entered Username: "+ 
+    this.signinForm.controls['username'].value+ 
+    " and password: "+ this.signinForm.controls['password'].value)
+    
+  };
+
+};
