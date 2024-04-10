ea5fc1f3ed212e061eb8b3e9c022987e47c1de2b
Bob Winter <39530207+winterb721@users.noreply.github.com>
2024-04-10 08:48:21 -0400

    added register and home components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SigninComponent],
+  imports: [RouterOutlet, SigninComponent, HomeComponent, RegisterComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
      {path:'', redirectTo: 'signin', pathMatch: 'full'},
-     {path:'signin', component:SigninComponent}
+     {path:'signin', component:SigninComponent},
+     {path:'home', component:HomeComponent},
+     {path:'register', component:RegisterComponent}
     ];
